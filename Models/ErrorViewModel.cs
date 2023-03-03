2d0a7fc84d2f8d21f1b0d1e35706100d673f2d66
Angus McDonald <angusmcdonald13@gmail.com>
2023-03-03 12:29:20 +1100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StockCSV.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
