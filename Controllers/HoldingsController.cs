397a88c10ce54a10beaca6e8b665831dc18f5f44
Angus McDonald <angusmcdonald13@gmail.com>
2023-04-28 20:09:20 +1000

    re-routed the trade and tax summary to another page on file upload submit. (seperate trades controller and views)

--- Controllers/HoldingsController.cs
+++ Controllers/HoldingsController.cs
@@ -32,8 +32,6 @@ namespace StockCSV.Controllers
             return View(_context.Holding.ToList());
         }
 
-
-
         [HttpPost]
         public async Task<IActionResult> Index(IFormFile file)
         {
@@ -53,115 +51,10 @@ namespace StockCSV.Controllers
             {
                 ViewBag.Message = "File Upload Failed";
             }
-            // get csv data to display
-            ViewData["Trades"] = CsvToList();
-
-            // calculate and show tax figures
-            ViewData["Total"] = TaxCalculator();
 
             return _context.Holding != null ? 
                           View(await _context.Holding.ToListAsync()) :
                           Problem("Entity set 'StockCSVContext.Holding'  is null.");
-        }
-
-        private List<Trade> CsvToList()
-        {
-            var uploadedFiles = @"C:\Users\angus\source\repos\StockCSV\UploadedFiles";
-            var path = Directory.GetFiles(uploadedFiles);
-            if (path.Length >= 1)
-            {
-                using (var streamReader = new StreamReader(path[0]))
-                {
-                    using (var csvReader = new CsvReader(streamReader, CultureInfo.InvariantCulture))
-                    {
-                        var records = csvReader.GetRecords<Trade>().ToList();
-                        return records;
-                    }
-                }
-            }
-            return new List<Trade>();
-        }
-
-        private double TaxCalculator()
-        {
-            foreach (var holding in _context.Holding)
-            {
-                if (holding.Units == 0)
-                {
-                    holding.Code = "null";
-                }
-            }
-            var records = CsvToList();
-            records.Reverse();
-            var total = 0.0;
-            foreach (var record in records)
-            {
-                // if sell do this,
-                // take amount off holdings and find the difference between buy and sell amount
-                // if holdings becomes negative or doesnt exist throw exception???
-                if (record.TradeType == "Sell")
-                {
-                    foreach (var holding in _context.Holding)
-                    {
-                        if (holding.Code == record.Code)
-                        {
-                            var recordUnits = record.Units;
-                            holding.Units -= recordUnits;
-                            if (holding.Units < 0)
-                            {
-                                throw new Exception("Insufficient holdings for trade type sale of" + holding.Code);
-                            }
-                            var costPrice = recordUnits * holding.AVGPrice;
-                            var saleValue = (record.Price * recordUnits) - (record.GST + record.Brokerage);
-                            var profitLoss = saleValue - costPrice;
-                            if (profitLoss > 0 && holding.PurchaseDate.AddMonths(12) < DateTime.Now)
-                            {
-                                profitLoss /= 2;
-                            }
-                            if (holding.Units == 0)
-                            {
-                                _context.Remove(holding);
-                            }
-                            // add 12 month holding discount later???
-                            total += profitLoss;
-                        }
-                        else 
-                        {
-
-                        }
-                    }
-                    _context.SaveChanges();
-                }
-
-                // if buy do this, 
-                // if record asx code == an existing holding asx code, add to current holding and adjust average price. otherwise create new holding.
-                else if (record.TradeType == "Buy")
-                {
-                    var adjusted = 0;
-                    foreach (var holding in _context.Holding)
-                    {
-                        if (holding.Code == record.Code && holding.Units > 0)
-                        {
-                            holding.Units += record.Units;
-                            var totalCostPrice = record.Consideration + (holding.Units * holding.AVGPrice);
-                            holding.AVGPrice = Math.Round(totalCostPrice / holding.Units, 2);
-                            var purchaseDate = record.PurchaseDate.ToDateTime(TimeOnly.MinValue);
-                            holding.PurchaseDate = purchaseDate;
-                            adjusted = 1; 
-                        }
-
-                    }
-                    if (adjusted == 0)
-                    {
-                        var purchaseDate = record.PurchaseDate.ToDateTime(TimeOnly.MinValue);
-                        var newHolding = new Holding(record.Code, record.Units, record.Price, purchaseDate);
-                        _context.Add(newHolding);
-                        _context.SaveChanges();
-                    }
-                }
-            }
-            ViewData["Holdings"] = _context.Holding;
-            return Math.Round(total, 2);
         }
 
         // GET: Holdings/Details/5
